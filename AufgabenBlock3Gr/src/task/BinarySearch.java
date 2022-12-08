c3f18d815acac90b3d7e9c6530a1d3dbadbd6b85
Kipping <maximilian.kipping@sap.com>
2022-12-08 11:34:14 +0100

    initial commit

--- AufgabenBlock3Gr/src/task/BinarySearch.java
+++ AufgabenBlock3Gr/src/task/BinarySearch.java
@@ -3,9 +3,25 @@ package task;
 import java.util.Arrays;
 
 public class BinarySearch {
+	
+	public static void bubbleSort(int[] array) {
+	    // Führe Bubble Sort auf dem Array durch
+	    int n = array.length;
+	    for (int i = 0; i < n - 1; i++) {
+	      for (int j = 0; j < n - i - 1; j++) {
+	        if (array[j] > array[j + 1]) {
+	          // Tausche die Elemente an den Indexen j und j+1
+	          int temp = array[j];
+	          array[j] = array[j + 1];
+	          array[j + 1] = temp;
+	        }
+	      }
+	    }
+	  }
+	
 	public static int binarySearch(int[] array, int element) {
-		// Sortiere das Array mit der sort-Methode aus der Arrays-Klasse
-		Arrays.sort(array); //eigentlich Bubblesort nutzen -> noch ändern
+		// Sortiere das Array mit dem bubbleSort-Algorithmus
+	    bubbleSort(array);
 
 		// Führe die binäre Suche durch
 		int low = 0;
